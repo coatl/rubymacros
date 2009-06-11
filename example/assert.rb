21f275fe62a0e611d63a83b9006df4f571cf6e79
Caleb Clausen <coatl@inforadical.net>
2009-06-10 18:12:37 -0700

    rewrote assert macro now that macros aren't postponed

--- example/assert.rb
+++ example/assert.rb
@@ -1,18 +1,14 @@
 #this file uses macros! won't parse in normal ruby
-$Debug=1
-if $Debug
-  macro assert(cond)
-    if RedParse::OpNode===cond and /\A[=!]=\Z/===cond.op
-      left,op,right=*cond
-      :(fail 'expected '+^left.unparse+"(==#{^left}) to be "+
-             ^op+" "+^right.unparse+"(==#{^right})" unless ^cond)    
-    else
-      :(fail "expected #{:(^^cond)}, but was not true" unless ^cond)
+macro assert(cond)
+    if $Debug
+      if RedParse::OpNode===cond and /\A[=!]=\Z/===cond.op
+        left,op,right=*cond
+        :(fail 'expected '+^left.unparse+"(==#{^left}) to be "+
+               ^op+" "+^right.unparse+"(==#{^right})" unless ^cond)    
+      else
+        :(fail "expected #{:(^^cond)}, but was not true" unless ^cond)
+      end
     end
-  end
-else
-  macro assert(cond)
-  end
 end
 
 
@@ -29,7 +25,7 @@ def test_assert
     assert(a==b) #oops, fails. msg="expected a(==1) to be == b(==2)"
   rescue Exception=>e
     assert("expected a(==1) to be == b(==2)"== e.message) #better be ok
-  else fail
+  else puts "exception expected but was not seen"
   end
 
   begin
@@ -37,6 +33,6 @@ def test_assert
   rescue Exception=>e
     assert("expected nil, but was not true"== e.message) #better be ok
     #ok, that message didn't make a lot of sense...
-  else fail
+  else puts "exception expected but was not seen"
   end
 end
--- example/assert_wrap.rb
+++ example/assert_wrap.rb
@@ -1,4 +1,5 @@
 require 'macro'
+$Debug=1
 Macro.require 'example/assert'
 
 test_assert
