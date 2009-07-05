f91d39ba05f7d304fb24f8adb9c315f8af5ce1da
Caleb Clausen <coatl@inforadical.net>
2009-07-04 22:03:19 -0700

    completely destroy $: hacks made unnecessry by doorman

--- lib/macro.rb
+++ lib/macro.rb
@@ -16,9 +16,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
-#$:.unshift "../redparse/lib"
-#warn "$: hacked up to find latest redparse"
-
 require 'rubygems'
 require 'redparse'
 require "macro/form"
